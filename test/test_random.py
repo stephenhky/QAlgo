175da2783a2cb86e85e87e3fdec71cc878fd1096
stephenhky <stephenhky@yahoo.com.hk>
2026-01-08 14:26:59 -0500

    debugging

--- test/test_random.py
+++ test/test_random.py
@@ -1,5 +1,7 @@
 
 from qiskit.circuit import QuantumCircuit
+from qiskit import transpile
+from qiskit_aer import StatevectorSimulator
 
 from qalgo.random import perfect_coin_gate
 
@@ -7,6 +9,7 @@ from qalgo.random import perfect_coin_gate
 def test_perfect_coin() -> None:
     qc = QuantumCircuit(1, 1)
     qc.append(perfect_coin_gate())
+    transpiled_qc = transpile(qc)
 
-
-
+    simulator = StatevectorSimulator()
+    simulation_results = simulator.run(transpiled_qc)
