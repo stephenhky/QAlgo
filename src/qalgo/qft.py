64760c43516228f8a90cd4f0951272c28ed1a061
stephenhky <stephenhky@yahoo.com.hk>
2026-03-18 18:14:58 -0400

    implemented QFT

--- src/qalgo/qft.py
+++ src/qalgo/qft.py
@@ -1,8 +1,19 @@
 
 # quantum Fourier transform
+# Qiskit convention: Little-Endian
 
+import numpy as np
 from qiskit.circuit import Gate, QuantumCircuit, QuantumRegister
 
 
 def QuantumFourierTransformGate(nbqubits: int) -> Gate:
-    pass
+    register = QuantumRegister(nbqubits)
+    qc = QuantumCircuit(register)
+
+    for i in range(nbqubits):
+        qc.h(register[i])
+        if i < nbqubits:
+            for j in range(i+1):
+                qc.cu(0.,0., np.pi/2**(i+1-j), 0.,register[i+1], register[j])
+
+    return qc.to_gate()
