6eda216e8616a5a7ca22efe43258eb67f93f9957
stephenhky <stephenhky@yahoo.com.hk>
2026-03-19 14:39:30 -0400

    removed classical bit from the circuit

--- test/test_qft.py
+++ test/test_qft.py
@@ -1,6 +1,6 @@
 
 import numpy as np
-from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit import QuantumCircuit, QuantumRegister
 from qiskit.quantum_info import Statevector
 
 from qalgo.qft import QuantumFourierTransformGate
@@ -8,10 +8,8 @@ from qalgo.qft import QuantumFourierTransformGate
 
 def test_2qubit_qft():
     qr = QuantumRegister(2)
-    cr = ClassicalRegister(2)
-    qc = QuantumCircuit(qr, cr)
+    qc = QuantumCircuit(qr)
     qc.append(QuantumFourierTransformGate(2), [qr[0], qr[1]])
-    qc.measure(qr, cr)
 
     statevector = Statevector(qc)
     np.testing.assert_array_almost_equal(
