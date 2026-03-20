3b0f399471b03f3dda25df16926bf002263577f2
stephenhky <stephenhky@yahoo.com.hk>
2026-03-20 10:25:16 -0400

    added junit tests for qiskit given QFT gate

--- test/test_qft.py
+++ test/test_qft.py
@@ -2,6 +2,7 @@
 import numpy as np
 from qiskit import QuantumCircuit, QuantumRegister
 from qiskit.quantum_info import Statevector
+from qiskit.circuit.library import QFTGate
 
 from qalgo.qft import QuantumFourierTransformGate
 
@@ -10,6 +11,18 @@ def test_2qubit_qft_1():
     qr = QuantumRegister(2)
     qc = QuantumCircuit(qr)
     qc.append(QuantumFourierTransformGate(2), [qr[0], qr[1]])
+
+    statevector = Statevector(qc)
+    np.testing.assert_array_almost_equal(
+        statevector.data,
+        np.array([1., 1., 1., 1.]) * 0.5
+    )
+
+
+def test_2qubit_qiskitqftgate_1():
+    qr = QuantumRegister(2)
+    qc = QuantumCircuit(qr)
+    qc.append(QFTGate(2), [qr[0], qr[1]])
 
     statevector = Statevector(qc)
     np.testing.assert_array_almost_equal(
@@ -39,3 +52,24 @@ def test_2qubit_qft_2():
         np.array([1., -0.5+0.5j, 0., -0.5-0.5j]) * np.sqrt(0.5)
     )
 
+
+def test_2qubit_qiskitqftgate_2():
+    qr = QuantumRegister(2)
+    qc = QuantumCircuit(qr)
+    qc.h(0)
+    qc.cx(0, 1)
+    qc.x(0)    # a singlet state at this point
+    singlet_statevector = Statevector(qc)
+
+    # apply the QFT gate
+    qc.append(QFTGate(2), [qr[0], qr[1]])
+    qft_statevector = Statevector(qc)
+
+    np.testing.assert_array_almost_equal(
+        singlet_statevector.data,
+        np.array([0., 1., 1., 0.]) * np.sqrt(0.5)
+    )
+    np.testing.assert_array_almost_equal(
+        qft_statevector.data,
+        np.array([1., -0.5 + 0.5j, 0., -0.5 - 0.5j]) * np.sqrt(0.5)
+    )
