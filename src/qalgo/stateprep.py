55af00331e4c66e9454865a13ddb1aa38f1c7d5e
stephenhky <stephenhky@yahoo.com.hk>
2026-03-27 09:26:41 -0400

    debugged

--- src/qalgo/stateprep.py
+++ src/qalgo/stateprep.py
@@ -6,6 +6,6 @@ def GHZState(nbqubits: int) -> Gate:
     qr = QuantumRegister(nbqubits)
     qc = QuantumCircuit(qr)
     qc.h(qr[0])
-    for i in range(1, nbqubits-1):
+    for i in range(0, nbqubits-1):
         qc.cx(qr[0], qr[i+1])
     return qc.to_gate()
