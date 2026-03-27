0db8b1faf17d8d55e326ba1a1bc5afd0d8f8f1e6
stephenhky <stephenhky@yahoo.com.hk>
2026-03-27 09:50:57 -0400

    4-qubit GHZ state test

--- test/test_stateprep.py
+++ test/test_stateprep.py
@@ -24,3 +24,16 @@ def test_3qubit_ghz_state():
         statevector.data,
         np.array([1., 0., 0., 0., 0., 0., 0., 1.]) * np.sqrt(0.5)
     )
+
+
+def test_4qubit_ghz_state():
+    qc = QuantumCircuit(4)
+    qc.append(GHZState(4), [0, 1, 2, 3])
+    statevector = Statevector(qc)
+    np.testing.assert_array_almost_equal(
+        statevector.data,
+        np.array([1., 0., 0., 0.,
+                  0., 0., 0., 0.,
+                  0., 0., 0., 0.,
+                  0., 0., 0., 1.]) * np.sqrt(0.5)
+    )
