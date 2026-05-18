5ee7d5bc9e7f00955050e0a02148f35ccd2ae8e3
stephenhky <stephenhky@yahoo.com.hk>
2026-05-18 12:55:44 -0400

    added inverse phase estimation gate

--- src/qalgo/phase.py
+++ src/qalgo/phase.py
@@ -29,3 +29,27 @@ def PhaseEstimationGate(oracle_gate: Gate, nbphasedigits: int, nbstatequbits: in
     #     [phase_qregisters[i] for i in range(nbphasedigits)]
     # )
     return qc.to_gate()
+
+
+def InversePhaseEstimationGate(oracle_gate: Gate, nbphasedigits: int, nbstatequbits: int) -> Gate:
+    controlled_oracle_gate = oracle_gate.control(1)
+
+    phase_qregisters = QuantumRegister(nbphasedigits)
+    state_qregisters = QuantumRegister(nbstatequbits)
+    qc = QuantumCircuit(phase_qregisters, state_qregisters)
+    qc.append(
+        QuantumFourierTransformGate(phase_qregisters.size, inverse=False),
+        [phase_qregisters[i] for i in range(nbphasedigits)]
+    )
+
+    repetitions = 1
+    for i in range(nbphasedigits):
+        qc.append(
+            controlled_oracle_gate.power(repetitions),
+            [phase_qregisters[i]] + [state_qregisters[j] for j in range(nbstatequbits)]
+        )
+        repetitions *= 2
+
+    qc.h(phase_qregisters)
+
+    return qc.to_gate()
