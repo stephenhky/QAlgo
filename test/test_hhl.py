a8ab9eff7e86dec1694141d4ef2f4cdf319e2a04
Kwan Yuet Stephen Ho <3810067+stephenhky@users.noreply.github.com>
2026-05-30 23:57:01 -0400

    new test

--- test/test_hhl.py
+++ test/test_hhl.py
@@ -1,10 +1,8 @@
 
 import numpy as np
-import numpy.typing as npt
-from qiskit.circuit import QuantumCircuit, QuantumRegister, ClassicalRegister
-from qiskit import transpile
-from qiskit_aer import StatevectorSimulator
-from loguru import logger
+import pytest
+from qiskit.circuit import QuantumCircuit, QuantumRegister
+from qiskit.quantum_info import Statevector
 
 from qalgo.hhl import HHLGate
 
@@ -19,19 +17,21 @@ def test_hhl_1():
     ancilla_register = QuantumRegister(1)
     clock_register = QuantumRegister(nb_clock)
     b_register = QuantumRegister(nb_b)
-    classical_register = ClassicalRegister(1)
-    qc = QuantumCircuit(ancilla_register, clock_register, b_register, classical_register)
+    qc = QuantumCircuit(ancilla_register, clock_register, b_register)
     qc.append(
         HHLGate(A, b, nb_b, nb_clock),
         [ancilla_register[0]] + [clock_register[i] for i in range(nb_clock)] + [b_register[i] for i in range(nb_b)]
     )
-    qc.measure(ancilla_register[0], classical_register)
 
-    nbshots = 1024
-    simulator = StatevectorSimulator(max_shot_size=1024)
-    transpiled_qc = transpile(qc, simulator, optimization_level=0)
-    job = simulator.run(transpiled_qc, shots=nbshots)
-    result = job.result()
-    count_dict = result.get_counts(qc)
-
-    logger.info(count_dict)
+    sv = Statevector(qc)
+    filtered_probs = {
+        str(key): float(prob)
+        for key, prob in sv.probabilities_dict().items()
+        if key[-1] == '1'
+    }
+    measured_b_proba = {
+        "0": sum(val for key, val in filtered_probs.items() if key[0]=="0"),
+        "1": sum(val for key, val in filtered_probs.items() if key[0]=="1")
+    }
+    assert measured_b_proba["1"] > measured_b_proba["0"]
+    assert measured_b_proba["1"] / measured_b_proba["0"] == pytest.approx((9.))
